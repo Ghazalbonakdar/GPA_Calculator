f1103f8d643f16320bcfac4ce6d8c401586d3ca3
Ghazal <ghazallbonakdar@gmail.com>
2024-12-07 15:04:54 +0330

    second commit

--- gpa_calc.py
+++ gpa_calc.py
@@ -3,7 +3,7 @@ def gpa_calc(number_subjects):
     total_units = 0
     for subjects in range(1,number_subjects+1):
         #taking the inputs
-        gpa_units = input().split()
+        gpa_units = input("Enter your gpa_units: ").split()
         gpa_units_int = list(map(int,gpa_units))
         gpa, units = gpa_units_int
 
